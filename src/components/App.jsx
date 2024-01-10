ecf2e259e1986889e6b946ebc6e4f202ac18ef4f
BekirYldrm <bekiryildirim.2508@gmail.com>
2024-01-10 19:09:47 +0300

    A function was created to use the data in the form.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Form from "./Form";
 
-function App(){
+function App() {
+
+    function Sumbittted(event) {
+        const latValue = event.target.latInput.value
+        const longValue = event.target.longInput.value
+        const radValue = event.target.radInput.value
+
+        event.preventDefault();
+
+    }
 
     return (
         <div>
-            <Form />
+            <Form handleSumbit={Sumbittted} />
         </div>
-        )
+    )
 
 }
 export default App;
