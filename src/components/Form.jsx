9b3cf70cd7704429cc5aa61ff03b33c3ff0a3c98
BekirYldrm <bekiryildirim.2508@gmail.com>
2024-01-10 19:11:56 +0300

    props added.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Input from "./Input";
 
-function Form () {
+function Form(props) {
+
+
     return (
         <div>
-            <form action="">
+            <form onSubmit={props.handleSumbit}>
+
                 <Input name="latInput" label="Latitude" />
-                <Input name="longInput" label="Longitude"/>
+
+                <Input name="longInput" label="Longitude" />
+
                 <Input name="radInput" label="Radius" />
+
                 <button type="submit">Search on Map</button>
-                
+
             </form>
         </div>
-        )
-
-}
+    );
+};
 
 export default Form;
